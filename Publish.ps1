843583eeaa88682f708737a3db3fe202ab68f329
Fu Wang <wangfu91@hotmail.com>
2021-09-26 12:13:16 +0800

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -1,7 +1,7 @@
 Push-Location .\UsnParser
 
-dotnet publish -c release -r win-x64 -o ../publish/win-x64
+dotnet publish -c release -r win-x64 -o ../publish/win-x64 --self-contained
 
-dotnet publish -c release -r win-x86 -o ../publish/win-x86
+dotnet publish -c release -r win-x86 -o ../publish/win-x86 --self-contained
 
 Pop-Location
