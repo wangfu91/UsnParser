9e4031ac9c80d0193c1a055681cca0947f831a6e
Fu Wang <5633310+wangfu91@users.noreply.github.com>
2024-07-26 18:15:58 +0800

    Save.

---
+++ UsnParser/Native/FILE_INTERNAL_INFORMATION.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsnParser.Native
+{
+    /// <summary>
+    ///  Used to get the reference number for a file. [FILE_INTERNAL_INFORMATION]
+    /// </summary>
+    /// <remarks>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/windows/hardware/ff540318.aspx"/>
+    /// </remarks>
+    public readonly struct FILE_INTERNAL_INFORMATION
+    {
+        /// <summary>
+        ///  Reference number for the file.
+        /// </summary>
+        public readonly long IndexNumber;
+    }
+}
---
+++ UsnParser/PathHelper.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using static UsnParser.Native.Kernel32;
+using static UsnParser.Native.Ntdll;
+using FileAccess = UsnParser.Native.FileAccess;
+using UsnParser.Native;
+using Microsoft.Win32.SafeHandles;
+using System.ComponentModel;
+using System.Text;
+
+namespace UsnParser
+{
+    internal static class PathHelper
+    {
+        private const string ExtendedPathPrefix = @"\\?\";
+
+        public static void Main(string[] args)
+        {
+            /*
+            var fid = 0x0000000000000000004900000006c2f5;
+            var path = PathFromFid("D:", fid);
+            Console.WriteLine($"path = {0}", path ?? "");
+            */
+
+            var path = @"D:\test.pdf";
+            var fid = GetFileIdFromPath(path);
+            Console.WriteLine($"fid = {fid}");
+        }
+
+        private static SafeFileHandle GetVolumeRootHandle(DriveInfo driveInfo)
+        {
+            var vol = string.Concat(@"\\.\", driveInfo.Name.TrimEnd('\\'));
+
+            var rootHandle = CreateFile(
+                vol,
+                FileAccess.GENERIC_READ | FileAccess.GENERIC_WRITE,
+                FileShare.ReadWrite,
+                default,
+                FileMode.Open,
+                0,
+                default
+                );
+
+            if (rootHandle.IsInvalid)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return rootHandle;
+        }
+
+        public unsafe static long? GetFileIdFromPath(string filePath)
+        {
+            filePath = ExtendedPathPrefix + filePath;
+            fixed (char* c = &MemoryMarshal.GetReference(filePath.AsSpan()))
+            {
+                UNICODE_STRING unicodeString = new UNICODE_STRING
+                {
+                    Length = checked((ushort)(filePath.Length * sizeof(char))),
+                    MaximumLength = checked((ushort)((filePath.Length + 1) * sizeof(char))),
+                    Buffer = (IntPtr)c
+                };
+
+                long fileId = 0L;
+
+                var objAttributes = new OBJECT_ATTRIBUTES
+                {
+                    length = (uint)sizeof(OBJECT_ATTRIBUTES),
+                    objectName = &unicodeString,
+                    rootDirectory = IntPtr.Zero,
+                    attributes = (int)ObjectAttribute.OBJ_CASE_INSENSITIVE
+                };
+
+                var status = NtCreateFile(
+                    handle: out var fileHandle,
+                    access: FileAccess.GENERIC_READ,
+                    objectAttributes: objAttributes,
+                    ioStatusBlock: out var ioStatusBlock,
+                    allocationSize: 0,
+                    fileAttributes: 0,
+                    shareAccess: FileShare.ReadWrite | FileShare.Delete,
+                    createDisposition: NtFileMode.FILE_OPEN,
+                    createOptions: NtFileCreateOptions.FILE_NON_DIRECTORY_FILE,
+                    eaBuffer: IntPtr.Zero,
+                    eaLength: 0);
+
+                using (fileHandle)
+                {
+                    if (status == STATUS_SUCCESS)
+                    {
+                        var internalInfoPtr = Marshal.AllocHGlobal(sizeof(FILE_INTERNAL_INFORMATION));
+                        try
+                        {
+                            status = NtQueryInformationFile(
+                                fileHandle,
+                                ioStatusBlock,
+                                internalInfoPtr,
+                                (uint)sizeof(FILE_INTERNAL_INFORMATION),
+                                FILE_INFORMATION_CLASS.FileInternalInformation);
+
+                            if (status == STATUS_SUCCESS)
+                            {
+                                var internalInfo = (FILE_INTERNAL_INFORMATION*)internalInfoPtr;
+                                fileId = internalInfo->IndexNumber;
+                            }
+                        }
+                        finally
+                        {
+                            Marshal.FreeHGlobal(internalInfoPtr);
+                        }
+                    }
+                }
+
+                return fileId;
+            }
+        }
+
+        public unsafe static string? PathFromFid(string volumeName, long fid)
+        {
+            string? path = null;
+            var driveInfo = new DriveInfo(volumeName);
+            var volumeRootHandle = GetVolumeRootHandle(driveInfo);
+
+            var unicodeString = new UNICODE_STRING
+            {
+                Length = sizeof(long),
+                MaximumLength = sizeof(long),
+                Buffer = new IntPtr(&fid)
+            };
+            var objAttributes = new OBJECT_ATTRIBUTES
+            {
+                length = (uint)sizeof(OBJECT_ATTRIBUTES),
+                objectName = &unicodeString,
+                rootDirectory = volumeRootHandle.DangerousGetHandle(),
+                attributes = (int)ObjectAttribute.OBJ_CASE_INSENSITIVE
+            };
+
+            var status = NtCreateFile(
+                out var fileHandle,
+                FileAccess.GENERIC_READ | FileAccess.GENERIC_WRITE,
+                objAttributes,
+                out var ioStatusBlock,
+                0,
+                0,
+                FileShare.ReadWrite | FileShare.Delete,
+                NtFileMode.FILE_OPEN,
+                NtFileCreateOptions.FILE_OPEN_BY_FILE_ID | NtFileCreateOptions.FILE_OPEN_FOR_BACKUP_INTENT,
+                IntPtr.Zero,
+                0);
+
+            using (fileHandle)
+            {
+                if (status == STATUS_SUCCESS)
+                {
+                    var pathBufferSize = MAX_PATH;
+                    while (true)
+                    {
+                        var pathBuffer = Marshal.AllocHGlobal(pathBufferSize);
+                        try
+                        {
+                            status = NtQueryInformationFile(
+                                fileHandle,
+                                ioStatusBlock,
+                                pathBuffer,
+                                (uint)pathBufferSize,
+                                FILE_INFORMATION_CLASS.FileNameInformation);
+                            if (status == STATUS_SUCCESS)
+                            {
+                                var nameInfo = (FILE_NAME_INFORMATION*)pathBuffer;
+
+                                path = Path.Join(volumeName.TrimEnd(Path.DirectorySeparatorChar), nameInfo->FileName.ToString());
+                            }
+                            else if (status == STATUS_INFO_LENGTH_MISMATCH || status == STATUS_BUFFER_OVERFLOW)
+                            {
+                                // The buffer size is not large enough to contain the name information,
+                                // increase the buffer size by a factor of 2 then try again.
+                                pathBufferSize *= 2;
+                                continue;
+                            }
+
+                            break;
+                        }
+                        finally
+                        {
+                            Marshal.FreeHGlobal(pathBuffer);
+                        }
+                    }
+                }
+            }
+
+            return path;
+        }
+    }
+}
